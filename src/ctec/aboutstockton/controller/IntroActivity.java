4f40e3d6b6e488c717106328acaa25c3193ef197
Stockton Slack <stocktonslack@gmail.com>
2014-10-21 22:15:58 -0600

    Adding to Repo
    About Stockton App from AI2 recreate

---
+++ src/ctec/aboutstockton/controller/IntroActivity.java
@@ -0,0 +1,15 @@
+package ctec.aboutstockton.controller;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class IntroActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+    }
+}
